948b6a64201270ad6d38ed46b47e17072a26bc98
catalinaruiu <catalinaruiu@yahoo.com>
2023-04-05 09:33:12 +0300

    created new spring project with added dependencies

---
+++ src/main/java/com/codecool/elprojectegrande/ElprojectegrandeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.elprojectegrande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElprojectegrandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElprojectegrandeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/elprojectegrande/ElprojectegrandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.elprojectegrande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElprojectegrandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
