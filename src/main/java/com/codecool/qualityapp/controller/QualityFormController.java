4b5aa7aebce1ba5bc5d0600eb09a9ee306096c6b
catalinaruiu <catalinaruiu@yahoo.com>
2023-04-07 02:40:15 +0300

    created GET mapping for displaying a list of all quality forms

--- src/main/java/com/codecool/qualityapp/controller/QualityFormController.java
+++ src/main/java/com/codecool/qualityapp/controller/QualityFormController.java
@@ -1,8 +1,26 @@
 package com.codecool.qualityapp.controller;
 
+import com.codecool.qualityapp.model.QualityForm;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class QualityFormController {
+    private List<QualityForm> allForms = new ArrayList<>();
+
+    public QualityFormController() {
+        // Initialize the list with some QualityForm objects
+        allForms.add(new QualityForm(1, 88.5, "Nicely done!"));
+        allForms.add(new QualityForm(2, 67.2, "Needs improvement."));
+        allForms.add(new QualityForm(3, 99.0, "Excellent work!"));
+    }
+
+    @GetMapping("/forms")
+    public List<QualityForm> getQualityForms() {
+        return allForms;
+    }
 
 }
