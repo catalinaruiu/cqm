1c3a61851c01628eb6f2f664055940fb8d2f772c
catalinaruiu <catalinaruiu@yahoo.com>
2023-04-06 23:49:02 +0300

    renamed package to match project

--- src/main/java/com/codecool/qualityapp/ElProjecteGrandeApplication.java
+++ src/main/java/com/codecool/qualityapp/ElProjecteGrandeApplication.java
@@ -1,4 +1,4 @@
-package com.codecool.elprojectegrande;
+package com.codecool.qualityapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
