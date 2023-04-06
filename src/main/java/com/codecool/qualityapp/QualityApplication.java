760fce5a8e1db42c829f1f736c592e5fad9c0ecf
catalinaruiu <catalinaruiu@yahoo.com>
2023-04-06 23:50:32 +0300

    renamed main class and its test class

--- src/main/java/com/codecool/qualityapp/QualityApplication.java
+++ src/main/java/com/codecool/qualityapp/QualityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElProjecteGrandeApplication {
+public class QualityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElProjecteGrandeApplication.class, args);
+		SpringApplication.run(QualityApplication.class, args);
 	}
 
 }
