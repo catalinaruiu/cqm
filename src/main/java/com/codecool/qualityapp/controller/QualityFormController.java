93e82efde48747d50fa407101458639cfb0e6cb2
catalinaruiu <catalinaruiu@yahoo.com>
2023-04-20 19:07:38 +0300

    code corrections after code review

--- src/main/java/com/codecool/qualityapp/controller/QualityFormController.java
+++ src/main/java/com/codecool/qualityapp/controller/QualityFormController.java
@@ -1,16 +1,13 @@
 package com.codecool.qualityapp.controller;
 
 import com.codecool.qualityapp.model.QualityForm;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/")
 public class QualityFormController {
     private List<QualityForm> allForms = new ArrayList<>();
 
@@ -21,28 +18,19 @@ public class QualityFormController {
         allForms.add(new QualityForm(3, 99.0, "Excellent work!"));
     }
 
-    @GetMapping("/forms")
+    @GetMapping
     public List<QualityForm> getQualityForms() {
         return allForms;
     }
 
-//    @PostMapping("/forms")
-//    public void create(@RequestBody QualityForm qualityForm) {
-//        allForms.add(qualityForm);
-//    }
-
-    @PostMapping("/new-form")
-    public ResponseEntity<String> addQualityForm() {
-        allForms.add(new QualityForm(allForms.size() + 1, 95.0, "Great job!"));
-        return ResponseEntity.ok("Quality form added successfully!");
+    @PostMapping
+    public QualityForm create(@RequestBody QualityForm qualityForm) {
+        allForms.add(qualityForm);
+        return qualityForm;
     }
 
-    @DeleteMapping("/forms/{id}")
+    @DeleteMapping("/{id}")
     public void deleteQualityForm(@PathVariable int id) {
-        Map<Integer, QualityForm> formsMap = allForms.stream()
-                .collect(Collectors.toMap(QualityForm::getId, Function.identity()));
-        formsMap.remove(id);
-        allForms.clear();
-        allForms.addAll(formsMap.values());
+        allForms.removeIf(f -> f.getId() == id);
     }
 }
