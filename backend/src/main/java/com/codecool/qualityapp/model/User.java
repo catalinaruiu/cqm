a9f6bb9a367bff15feb65da69316fe7f44246826
catalinaruiu <catalinaruiu@yahoo.com>
2023-05-18 22:27:56 +0300

    added constructor, getters and setters for the User class

--- backend/src/main/java/com/codecool/qualityapp/model/User.java
+++ backend/src/main/java/com/codecool/qualityapp/model/User.java
@@ -6,4 +6,52 @@ public class User {
     private String emailAddress;
     private String project;
     private Position position;
+
+    public User(String firstName, String lastName, String emailAddress, String project, Position position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.project = project;
+        this.position = position;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
